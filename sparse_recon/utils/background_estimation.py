566929a969f5136f0c198eeb84e00bd8c9452e39
WeisongZhao <zhaoweisong950713@163.com>
2022-12-12 21:33:56 +0800

    corrected background

--- sparse_recon/utils/background_estimation.py
+++ sparse_recon/utils/background_estimation.py
@@ -1,13 +1,39 @@
-import pywt
+
 import numpy as np
-from numpy import zeros
+import matplotlib.pyplot as plt
+import pywt
+from skimage.io import imread, imsave, imshow
+
+def Low_frequency_resolve(coeffs, dlevel):
+    cAn = coeffs[0]
+    vec = []
+    vec.append(cAn)
+    for i in range(1, dlevel+1):
+        (cH, cV, cD) = coeffs[i]
+        [cH_x, cH_y] = cH.shape
+        cH_new = np.zeros((cH_x, cH_y))
+        t = (cH_new, cH_new, cH_new)
+        vec.append(t)
+    return vec
 
 
-def background_estimation(imgs, th = 1, dlevel = 6, wavename = 'db6', iter = 3):
-    img1 = imgs
+def rm_1(Biter, x, y):
+    Biter_new = np.zeros((x, y), dtype=('uint8'))
+    if x%2 and y%2 == 0:
+        Biter_new[:, :] = Biter[0:x, :]
+    elif x%2 == 0 and y%2:
+        Biter_new[:, :]  = Biter[:, 0:y]
+    elif x%2 and y%2:
+        Biter_new[:, :]  = Biter[0:x, 0:y]
+    else:
+        Biter_new = Biter
+    return Biter_new
+
+
+def background_estimation_stack(imgs, th = 1, dlevel = 7, wavename = 'db6', iter = 3):
     ''' Background estimation
         function Background = background_estimation(imgs,th,dlevel,wavename,iter)
-        ims: ndarray
+        imgs: ndarray
             Input image (can be N dimensional).
         th : int, optional
             if iteration {default:1}
@@ -18,82 +44,47 @@ def background_estimation(imgs, th = 1, dlevel = 6, wavename = 'db6', iter = 3):
         iter:  int, optional
          iteration {default:3}
         -----------------------------------------------
-        Output:
+        Return:
          Background
     '''
-    if imgs.ndim < 3:
-
-        [x, y] = imgs.shape
-
-        z = 1
-        if x != y:
-            if x < y:
-                imgs = np.lib.pad(imgs, ((max(x, y) - imgs.shape[0], 0),(0,max(x, y) - imgs.shape[1])), 'symmetric')
-            else:
-                imgs = np.lib.pad(imgs, ((0,max(x, y) - imgs.shape[0]),(max(x, y) - imgs.shape[1],0)), 'symmetric')
-        Background = np.zeros((img1.shape[0], img1.shape[1]), dtype = 'float32')
-    else:
-        [z, x,y] = imgs.shape
-        print(max(x, y) - imgs.shape[1])
-        print(max(x, y) - imgs.shape[2])
-        if x != y:
-            if x < y:
-                imgs = np.lib.pad(imgs, ((0,0),(max(x, y) - imgs.shape[1], 0), (0, max(x, y) - imgs.shape[2])), 'symmetric')
-            else:
-                imgs = np.lib.pad(imgs, ((0,0),(0, max(x, y) - imgs.shape[1]), (max(x, y) - imgs.shape[2], 0)), 'symmetric')
-        Background = np.zeros((img1.shape[0],img1.shape[1],img1.shape[2]), dtype = 'float32')
-    for frames in range(0,z):
-        if imgs.ndim < 3:
+    try:
+        [t, x, y] = imgs.shape 
+        Background = np.zeros((t, x, y))
+        for taxial in range(t):
+            img = imgs[taxial, :, :]
+            for i in range(iter):
+                initial = img
+                res = initial
+                coeffs = pywt.wavedec2(res, wavelet = wavename, level = dlevel)
+                vec = Low_frequency_resolve(coeffs, dlevel)
+                Biter = pywt.waverec2(vec, wavelet = wavename)
+                Biter_new = rm_1(Biter, x, y)
+                if th > 0:
+                    eps = np.sqrt(np.abs(res))/2
+                    ind = initial>(Biter_new+eps)
+                    res[ind] = Biter_new[ind]+eps[ind]
+                    coeffs1 = pywt.wavedec2(res, wavelet = wavename, level = dlevel)
+                    vec = Low_frequency_resolve(coeffs1, dlevel)
+                    Biter =  pywt.waverec2(vec, wavelet = wavename)
+                    Biter_new = rm_1(Biter, x, y)
+                    Background[taxial, :, :] = Biter_new
+    except ValueError:
+        [x, y] = imgs.shape 
+        Background = np.zeros((x, y))
+        for i in range(iter):
             initial = imgs
-        else:
-            initial = imgs[frames,:,:]
-        res = initial
-        for ii in range(0,iter):
-
-            m =pywt.wavedec2(res, wavename,'symmetric', dlevel)
-            k = pywt.wavedec2(res, wavename, 'symmetric', dlevel)
-            #print(k[1][2].shape)
-            list_out = []
-            for i in k:
-                lt = list(i)
-                list_out.append(lt)
-
-            n = np.zeros((dlevel + 2, 2))
-            for g in range(0,dlevel + 1):
-                n[g,:] = np.array(m[g][1].shape)
-            n[dlevel + 1,:] = np.array(initial.shape)
-            for kk in range(1,dlevel+1):
-                list_out[kk][0] = zeros((int(n[kk,1]), int(n[kk,1])), dtype = 'float32')
-                list_out[kk][1] = zeros((int(n[kk,1]), int(n[kk,1])), dtype = 'float32')
-                list_out[kk][2] = zeros((int(n[kk,1]), int(n[kk,1])), dtype = 'float32')
-            Biter = pywt.waverec2(list_out, wavename)
-
+            res = initial
+            coeffs = pywt.wavedec2(res, wavelet = wavename, level = dlevel)
+            vec = Low_frequency_resolve(coeffs, dlevel)
+            Biter = pywt.waverec2(vec, wavelet = wavename)
+            Biter_new = rm_1(Biter, x, y)
             if th > 0:
                 eps = np.sqrt(np.abs(res))/2
-                ind = initial > (Biter + eps)
-                res[ind] = Biter[ind] + eps[ind]
-                k = pywt.wavedec2(res, wavename,'symmetric',dlevel)
-                list_out = []
-                for i in k:
-                    lt = list(i)
-                    list_out.append(lt)
-                for kk in range(1, dlevel + 1):
-                    list_out[kk][0] = zeros((int(n[kk, 1]),int(n[kk, 1])), dtype = 'float32')
-                    list_out[kk][1] = zeros((int(n[kk, 1]),int(n[kk, 1])), dtype = 'float32')
-                    list_out[kk][2] = zeros((int(n[kk, 1]), int(n[kk, 1])), dtype = 'float32')
-                Biter = pywt.waverec2(list_out, wavename)
-
-        if imgs.ndim < 3:
-            if x != y:
-                if x < y:
-                    Background = Biter[y - x:y, 0:y]
-                else:
-                    Background = Biter[0:x, x - y:x]
-        else:
-            #Background[frames,:,:] = Biter
-            if x != y:
-                if x < y:
-                    Background[frames, :, :] = Biter[y - x:y, 0:y]
-                else:
-                    Background[frames, :, :] = Biter[0:x, x - y:x]
-    return  Background
+                ind = initial>(Biter_new+eps)
+                res[ind] = Biter_new[ind]+eps[ind]
+                coeffs1 = pywt.wavedec2(res, wavelet = wavename, level = dlevel)
+                vec = Low_frequency_resolve(coeffs1, dlevel)
+                Biter =  pywt.waverec2(vec, wavelet = wavename)
+                Biter_new = rm_1(Biter, x, y)
+                Background = Biter_new
+    return Background
